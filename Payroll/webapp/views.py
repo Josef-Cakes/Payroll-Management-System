051fc14f5f54ac66bce918f1ef8c72ac19720e77
Josef-Cakes <exoruexot@gmail.com>
2024-11-30 02:14:39 +0800

    views

--- Payroll/webapp/views.py
+++ Payroll/webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from .models import Employee, Admin, Salary, Deduction
+from .forms import LoginForm
 
 # Home view
 def home(request):
@@ -10,17 +11,20 @@ def home(request):
 # Login view
 def login_view(request):
     if request.method == "POST":
-        print(request.POST)
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password = password)
-        if user is not None:
-            login(request, user)
-            return redirect('hr_dashboard')
-        else:
-            return redirect('hr_dashboard') 
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('hr_dashboard')
+            else:
+                return redirect('home') 
     else:
-        return render(request, 'index.html', {})
+        form = LoginForm()
+
+    return render(request, 'index.html', {'form' : form})
         
 
 
